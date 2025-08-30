9df17e01a402618c512c980c38678ba4132e3e48
Kashchit <kashchit10@gmail.com>
2025-08-30 23:37:08 +0530

    added some blogModel schema for the DB

---
+++ model/blogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    title :{
+        type : String
+    },
+    subtitle : {
+        type : String
+    },
+    description : {
+        type : Text 
+    },
+    image : {
+        type : String 
+    }
+    
+})
+
+const Blog = mongoose.model('Blog',blogSchema ) //making table first name of the table i.e blog and the schema 
+module.exports = Blog
