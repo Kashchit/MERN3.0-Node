22a9c878a17a8e16cc4c2097aef0b304c38eb570
Kashchit <kashchit10@gmail.com>
2025-08-31 16:01:38 +0530

    backend

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
+
 const app = express()
+app.use(express.json())
 
 connectToDatabase()
 
@@ -11,9 +13,10 @@ app.get("/",(req,res)=>{
     })
 })
 
-app.get("/about",(req,res)=>{
-    res.json({
-        message: "This is about page"
+app.post("/blog",(req,res)=>{
+    console.log(req.body)
+    res.status(200).json({
+        message : "Blog API hit successfully"
     })
 })
 
